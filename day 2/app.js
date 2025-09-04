c8fc6ae676b6bf7a6cee485411516c6ab992e835
tamanna <tamanna.lisbon@gmail.com>
2025-09-04 23:09:17 +0100

    i added js file,index html day 2 class history

---
+++ day 2/app.js
@@ -0,0 +1,23 @@
+
+
+console.log("hello");
+ var test; //creat a store
+test = "khulna";//assign data 
+console.log(test);
+
+var city;
+city = "dhaka";
+console.log(city);
+test1 = "no keyword";
+console.log(test1);
+
+const test = "this is test file"
+document.write(test);
+
+window.alert(test); 
+
+var country = "bangladesh";
+
+
+console.log(country);
+
