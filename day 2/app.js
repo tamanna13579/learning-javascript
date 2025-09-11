6e05c867453a2da21138462e034dd3e5cdf7e7db
tamanna <tamanna.lisbon@gmail.com>
2025-09-11 16:51:11 +0100

    restored my files

--- day 2/app.js
+++ day 2/app.js
@@ -1,5 +1,5 @@
 
-
+/*
 console.log("hello");
  var test; //creat a store
 test = "khulna";//assign data 
@@ -11,16 +11,82 @@ console.log(city);
 test1 = "no keyword";
 console.log(test1);
 
-const test = "this is test file"
+//const test = "this is test file"
 document.write(test);
 
-window.alert(test); 
+//window.alert(test); 
 
-var country = "bangladesh";
+
+var country ="india";
 
 
 console.log(country);
 
+console.log()  */
 
 
+//day 4
 
+//## operators & expressoins
+
+//### operators ,operands,expressions
+
+//arithmatic operator
+
+
+/*const num1 = 60;
+
+const num2 = 20;
+/*
+console.log(num1+num2+200);
+
+console.log(num1-num2);
+
+console.log(num1*num2);
+
+console.log(num1/num2);
+
+console.log(2**3);
+
+console.log(num1%num2); */
+
+
+/*let val = 30;
+val = val+1;
+console.log(val);
+
+val++; //value er sathe extra kore man barano hoy ++ sign diye
+
+
+val--; // minus sign diye value theke man komano hoy
+
+console.log(val);
+
+console.log(++val);
+
+console.log(val++);
+
+
+let x = 60;
+x+=60;
+
+//x+=60  means x = x+60 .
+*/
+
+// comperison assignment,boolean
+
+
+const num1 =60;
+const num2=20;
+
+console.log(100>99);
+
+console.log(num2>num1);
+console.log(99==99);
+
+console.log("99"==99);
+
+//javascript can change data type , string k number e convert kore dey .
+
+console.log("99"===99);
+// when 3 equal time deya hoy tokhon javascript ar change kore na nij theke
